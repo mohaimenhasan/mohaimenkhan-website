d3e6500bf86ddb9149da9290ec2707b2313b726a
Mohaimen Khan <mohaimenkhan@microsoft.com>
2025-03-13 01:05:46 -0700

    fix: update favicon link path to include 'portfolio' directory in Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -15,7 +15,7 @@ const Layout = ({ children, title = 'Mohaimen Khan - Senior Software Engineer', 
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/portfolio/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
